5765c0c2fba08ec993f546d0737fd767356fc5d0
El missouri <elmissouri16@gmail.com>
2018-04-23 21:35:08 +0100

    first init

---
+++ index.android.js
@@ -0,0 +1,31 @@
+"use strict";
+var mime = require('mim');
+var application = require("application");
+var getContext = function () {
+    if (application.android.context) {
+        return (application.android.context);
+    }
+    var ctx = java.lang.Class.forName("android.app.AppGlobals").getMethod("getInitialApplication", null).invoke(null, null);
+    if (ctx) return ctx;
+
+    ctx = java.lang.Class.forName("android.app.ActivityThread").getMethod("currentApplication", null).invoke(null, null);
+    return ctx;
+};
+
+var openFile = function (FilePath) {
+    try {
+        var file_type = mime.getMIMEType(FilePath);
+        if (FilePath === false) {
+            console.log('File Format Not Available');
+        } else {
+            var intent = new android.content.Intent(android.content.Intent.ACTION_VIEW);
+            intent.setDataAndType(android.net.Uri.fromFile(new java.io.File(FilePath)), file_type);
+            application.android.currentContext.startActivity(android.content.Intent.createChooser(intent, "Open File..."));
+        }
+
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+module.exports = openFile;
